version2:implement edge-guided masking and fix main_pretrain.py

engine_pretrain.py
@@ -1,4 +1,5 @@
-# 直接来自源代码 https://github.com/facebookresearch/mae.git
+# 来自源代码 https://github.com/facebookresearch/mae.git
+# 修改使其适用于my_mae
 # --------------------------------------------------------
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
@@ -47,7 +48,8 @@ def train_one_epoch(model: torch.nn.Module,
         samples = samples.to(device, non_blocking=True)
 
         with torch.cuda.amp.autocast():
-            loss, _, _ = model(samples, mask_ratio=args.mask_ratio)
+            # 修改模型输入和输出
+            loss, mean, var, pred, mask = model(samples)
 
         loss_value = loss.item()
 

main_pretrain.py
@@ -1,5 +1,5 @@
 # 主要代码来源https://github.com/facebookresearch/mae.git
-# 修改部分由AI生成，只增加了Wrapper等让自定义模型与源代码适配
+# 修改部分适应自定义模型
 # -------------------------------------------------------------------#
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
@@ -38,53 +38,38 @@ from engine_pretrain import train_one_epoch
 from my_vit import MyVit
 from my_mae import MyMaskedAutoencoder
 
-# ----- 接口适配器 (Adapter Pattern) -----
-class MyMAEAdapter(nn.Module):
-    """
-    这是一个适配器类，用于弥合官方引擎(main_pretrain.py)与自定义模型之间的接口差异。
-    """
-    def __init__(self, norm_pix_loss=True):
-        super().__init__()
-        # 实例化ViT Encoder，使用Base规格
-        encoder = MyVit(img_size=224, patch_size=16, embed_dim=768, depth=12, num_heads=12)
-
-        self.model = MyMaskedAutoencoder(
-            encoder=encoder,
-            decoder_dim=512,
-            decoder_depth=8,
-            decoder_num_heads=16
-        )
-
-    def forward(self, imgs, mask_ratio=0.75):
-        self.model.mask_ratio = mask_ratio
-
-        loss, mean, var, pred, mask = self.model(imgs)
-
-        return loss, pred, mask
-# ---------------------------------------------
-
-
 def get_args_parser():
-    parser = argparse.ArgumentParser('MAE pre-training', add_help=False)
+    # 修改参数
+    parser = argparse.ArgumentParser('MyMae pre-training', add_help=False)
+
+    parser.add_argument('--img_size', default=224, type=int,
+                        help='images input size')
+    parser.add_argument('--patch_size', default=16, type=int,
+                        help='patch size of the ViT model')
     parser.add_argument('--batch_size', default=64, type=int,
                         help='Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus')
+    parser.add_argument('--in_chans',default=3,type=int)
+    parser.add_argument('--encoder_dim',default=768,type=int)
+    parser.add_argument('--encoder_depth',default=16,type=int)
+    parser.add_argument('--encoder_num_heads',default=16,type=int)
+    parser.add_argument('--encoder_mlp_ratio',default=4.,type=float)
+
+    parser.add_argument('--mask_type', default='random', type=str,
+                        help='Type of masking method to train')
+    parser.add_argument('--mask_ratio', default=0.75, type=float,
+                        help='Masking ratio (percentage of removed patches).')
+    parser.add_argument('--norm_pix_loss', action='store_true',
+                        help='Use (per-patch) normalized pixels as targets for computing loss')
+    parser.set_defaults(norm_pix_loss=False)
+
+    parser.add_argument('--decoder_dim',default=512,type=int)
+    parser.add_argument('--decoder_depth',default=6,type=int)
+    parser.add_argument('--decoder_num_heads',default=16,type=int)
+    parser.add_argument('--decoder_mlp_ratio',default=4.,type=float)
+
     parser.add_argument('--epochs', default=400, type=int)
     parser.add_argument('--accum_iter', default=1, type=int,
                         help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
-
-    # Model parameters (这里默认保持不变，实际上适配器内部已经写死了模型)
-    parser.add_argument('--model', default='my_mae_vit_base_patch16', type=str, metavar='MODEL',
-                        help='Name of model to train')
-
-    parser.add_argument('--input_size', default=224, type=int,
-                        help='images input size')
-
-    parser.add_argument('--mask_ratio', default=0.75, type=float,
-                        help='Masking ratio (percentage of removed patches).')
-
-    parser.add_argument('--norm_pix_loss', action='store_true',
-                        help='Use (per-patch) normalized pixels as targets for computing loss')
-    parser.set_defaults(norm_pix_loss=True) # 默认开启标准化Trick
 
     # Optimizer parameters
     parser.add_argument('--weight_decay', type=float, default=0.05,
@@ -152,7 +137,7 @@ def main(args):
 
     # simple augmentation
     transform_train = transforms.Compose([
-        transforms.RandomResizedCrop(args.input_size, scale=(0.2, 1.0), interpolation=3),  # 3 is bicubic
+        transforms.RandomResizedCrop(args.img_size, scale=(0.2, 1.0), interpolation=3),  # 3 is bicubic
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
@@ -182,11 +167,18 @@ def main(args):
         pin_memory=args.pin_mem,
         drop_last=True,
     )
-
-    # ---------------------------------------------------------
-    # 直接使用自定义的 Adapter 实例化模型，取代官方基于字符串的字典查找
-    # ---------------------------------------------------------
-    model = MyMAEAdapter(norm_pix_loss=args.norm_pix_loss)
+    # 修改模型
+    encoder = MyVit(img_size=args.img_size, patch_size=args.patch_size, embed_dim=args.encoder_dim, depth=args.encoder_depth, num_heads=args.encoder_num_heads)
+    model=MyMaskedAutoencoder(
+        encoder=encoder,
+        mask_type=args.mask_type,
+        mask_ratio=args.mask_ratio,
+        decoder_dim=args.decoder_dim,
+        decoder_depth=args.decoder_depth,
+        decoder_num_heads=args.decoder_num_heads,
+        decoder_mlp_ratio=args.decoder_mlp_ratio,
+        norm_pix_loss=args.norm_pix_loss
+    )
 
     model.to(device)
 
@@ -222,7 +214,6 @@ def main(args):
         if args.distributed:
             data_loader_train.sampler.set_epoch(epoch)
 
-        # 官方引擎调用训练，此时会调用 MyMAEAdapter
         train_stats = train_one_epoch(
             model, data_loader_train,
             optimizer, device, epoch, loss_scaler,

my_mae.py
@@ -4,10 +4,12 @@ import torch.nn as nn
 from my_vit import MyBlock
 # 直接导入mae源代码写好的util来生成固定的2D正余弦位置编码
 from util.pos_embed import get_2d_sincos_pos_embed
+from my_util.edge_ops import SobelPatchScorer
 
 class MyMaskedAutoencoder(nn.Module):
-    def __init__(self, encoder, mask_type='random',mask_ratio=0.75, decoder_dim=512, decoder_depth=8, decoder_num_heads=16, decoder_mlp_ratio=4.):
+    def __init__(self, encoder, mask_type='random',mask_ratio=0.75, decoder_dim=512, decoder_depth=8, decoder_num_heads=16, decoder_mlp_ratio=4.,norm_pix_loss=False):
         super().__init__()
+        self.norm_pix_loss=norm_pix_loss
         # Wrapper模式，encoder直接使用MyVit，用于下游任务时可以提取出encoder直接使用
         self.encoder=encoder
         num_patches=encoder.num_patches
@@ -30,6 +32,8 @@ class MyMaskedAutoencoder(nn.Module):
         self.mask_ratio=mask_ratio
         # mask掉的patch输入decoder都会表示为这个统一的mask token，维度[1,1,decoder_dim]
         self.mask_token=nn.Parameter(torch.zeros(1,1,decoder_dim))
+        # edge-guided masking需要用到的边缘打分器
+        self.edge_scorer = SobelPatchScorer(patch_size=patch_size)
 
         # 从encoder高维降到decoder低维的线性层
         self.enc_to_dec=nn.Linear(encoder_dim,decoder_dim) if encoder_dim != decoder_dim else nn.Identity()
@@ -38,7 +42,7 @@ class MyMaskedAutoencoder(nn.Module):
         self.decoder=nn.ModuleList([
             MyBlock(decoder_dim, decoder_num_heads, decoder_mlp_ratio)
             for _ in range(decoder_depth)])
-        self.decoder_norm=nn.LayerNorm(decoder_dim)
+        self.decoder_norm=nn.LayerNorm(decoder_dim, eps=1e-6)
         # decoder的位置编码，维度[1,num_patches+1,decoder_embedDim],同样是固定编码，不可学习所以不需要计算梯度
         self.decoder_pos_embed = nn.Parameter(torch.from_numpy(dec_pos_embed_np).float().unsqueeze(0), requires_grad=False)
         # 将decoder输出的latent represenation转化为原始像素的头
@@ -50,13 +54,15 @@ class MyMaskedAutoencoder(nn.Module):
         # 全局初始化，复用在MyVit写过的初始化函数
         self.apply(self.encoder._init_weights)
 
-    def masking(self, x, mask_type='random', mask_ratio=0.75):
+    def masking(self, imgs, x, mask_type='random', mask_ratio=0.75):
         # 根据不同的masking策略调用不同的函数
         strategies = {
             'random': self.random_masking,
             'attn': self.attn_guided_masking,
             'edge': self.edge_guided_masking
         }
+        if mask_type=='edge':
+            return strategies[mask_type](imgs, x, mask_ratio)
         return strategies[mask_type](x, mask_ratio)
 
     def random_masking(self, x, mask_ratio):
@@ -87,22 +93,57 @@ class MyMaskedAutoencoder(nn.Module):
 
         return x_keep,mask,ids_restore
 
+    def edge_guided_masking(self, imgs, x, mask_ratio):
+        # 扩展1：边缘引导掩码
+        batch_size, num_patches, encoder_dim = x.shape
+        num_keep = int((1 - mask_ratio) * num_patches)
+
+        # 1 计算边缘得分 [B, L]
+        with torch.no_grad():
+            scores = self.edge_scorer(imgs)
+
+            # 将“边缘得分”与“随机噪声”进行加权混合，增强泛化能力，并且随机保留一些重要的边缘块给模型学习
+            # 边缘得分归一化
+            scores_min = scores.min(dim=-1, keepdim=True)[0]
+            scores_max = scores.max(dim=-1, keepdim=True)[0]
+            norm_scores = (scores - scores_min) / (scores_max - scores_min + 1e-6)
+
+            alpha = 0.5  # 这是一个可以消融的超参数，控制边缘引导的强度
+            combined_scores = (1 - alpha) * torch.rand_like(scores) + alpha * norm_scores
+
+        # 2 根据综合得分进行排序
+        ids_shuffle = torch.argsort(combined_scores, dim=1)  # 升序排列
+        ids_keep = ids_shuffle[:, :num_keep]
+        ids_restore = torch.argsort(ids_shuffle, dim=1)
+
+        # 3 提取保留下来的特征块
+        ids_keep_expanded = ids_keep.unsqueeze(-1).repeat(1, 1, encoder_dim)
+        x_keep = torch.gather(x, dim=1, index=ids_keep_expanded)
+
+        mask = torch.ones([batch_size, num_patches], device=x.device)
+        mask[:, :num_keep] = 0
+        mask = torch.gather(mask, dim=1, index=ids_restore)
+
+        return x_keep, mask, ids_restore
+
     def attn_guided_masking(self, x, mask_ratio):
         # 扩展1：注意力引导掩码
+        # 1 提取，warmup得到初步的自注意力矩阵
+
+        # 2 计算，对每个patch的注意力进行计算
+
+        # 3 排序，根据计算结果排序，决定保留部分
+
         pass
 
-    def edge_guided_masking(self, x, mask_ratio):
-        # 扩展2：边缘引导掩码
-        pass
-
-    def forward_loss(self, imgs, pred, mask,norm_pix_loss=True):
+    def forward_loss(self, imgs, pred, mask):
         """
         计算loss的函数，直接修改使用原论文代码
         norm_pix_loss=True时使用原论文提出的 提升性能的Trick——目标像素标准化
         """
         # 只切块
         target = self.encoder.patch_embed[0](imgs)
-        if norm_pix_loss:
+        if self.norm_pix_loss:
             mean = target.mean(dim=-1, keepdim=True)
             var = target.var(dim=-1, keepdim=True)
             target = (target - mean) / (var + 1.e-6)**.5
@@ -126,7 +167,7 @@ class MyMaskedAutoencoder(nn.Module):
         x=x+self.encoder.pos_embed[:,1:,:]
 
         # 3 掩码,返回的x是保留后的块的tokens+pos_embed以及ids_restore(用于后续还原)
-        x,mask,ids_restore=self.masking(x,mask_type=self.mask_type,mask_ratio=self.mask_ratio)
+        x,mask,ids_restore=self.masking(imgs,x,mask_type=self.mask_type,mask_ratio=self.mask_ratio)
         L_keep=x.shape[1]
         L_mask=num_patches-L_keep
 
@@ -173,7 +214,7 @@ class MyMaskedAutoencoder(nn.Module):
         pred=self.to_pixels(decoder_out[:,1:,:])
 
         # 11 计算loss
-        loss, mean, var = self.forward_loss(imgs, pred, mask, norm_pix_loss=True)
+        loss, mean, var = self.forward_loss(imgs, pred, mask)
 
         # # 12 反标准化,把模型预测的标准化像素，乘回方差，加回均值 #放到外部实现，训练过程中不需要每次都进行这一步
         # pred_unnorm = pred * (var + 1.e-6)**.5 + mean

my_vit.py
@@ -8,13 +8,13 @@ class MyBlock(nn.Module):
     def __init__(self,embed_dim, num_heads, mlp_ratio):
         super().__init__()
         # pre-norm 把输入的数据拉回正态分布，防止梯度爆炸或消失
-        self.norm1=nn.LayerNorm(embed_dim)
+        self.norm1=nn.LayerNorm(embed_dim, eps=1e-6)
 
         # MultiheadAttention 全局信息交流
-        self.attn=nn.MultiheadAttention(embed_dim,num_heads,batch_first=True)
+        self.attn=nn.MultiheadAttention(embed_dim,num_heads,batch_first=True,bias=True)
 
         # norm2 为进入 MLP 做准备，再次稳定数据
-        self.norm2=nn.LayerNorm(embed_dim)
+        self.norm2=nn.LayerNorm(embed_dim, eps=1e-6)
 
         # MLP 内部信息交流，Token 内部的特征升维与提炼
         hidden_dim=int(embed_dim*mlp_ratio)
@@ -28,7 +28,7 @@ class MyBlock(nn.Module):
         # 归一化
         x_norm=self.norm1(x)
         # 计算Attention
-        attn_out,_=self.attn(x_norm,x_norm,x_norm)
+        attn_out,attn_w=self.attn(x_norm,x_norm,x_norm)
         # 残差连接
         x=x+attn_out
         # 归一化
@@ -74,7 +74,7 @@ class MyVit(nn.Module):
             MyBlock(embed_dim, num_heads, mlp_ratio)
              for _ in range(depth)])
         # 最终的归一化层
-        self.norm=nn.LayerNorm(embed_dim)
+        self.norm=nn.LayerNorm(embed_dim, eps=1e-6)
         # 如果是全局表征任务，比如分类任务，则返回线性层，其中num_classes>0是输出的维度
         self.head=nn.Linear(embed_dim,num_classes) if num_classes>0 else None
 